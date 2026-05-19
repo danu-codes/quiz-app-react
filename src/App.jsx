581030a965309afc89f3847bb17c7239024e9e75
Sri_Danushan <mailtodanu07@gmail.com>
2026-05-19 09:41:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import './App.css'
 import QuestionData from "./question.json"
 
 function App() {
+  const [startQuiz, setStartQuiz] = useState(false);
   const [currrentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [timer, setTimer] = useState(10);
 
+  const handleStartQuiz=()=>{
+    setStartQuiz(true);
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+    setTimer(10);
+  }
+
   useEffect(() => {
-    if (showScore) return;
+    if (showScore || !startQuiz) return;
 
     const interval = setInterval(() => {
       setTimer((prev) => {
@@ -23,7 +32,7 @@ function App() {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [showScore]);
+  }, [showScore, startQuiz]);
 
   const handleAnswerClick = (selectedOption) => {
     if (selectedOption == QuestionData[currrentQuestion].answer) {
@@ -46,27 +55,54 @@ function App() {
     setTimer(10);
   }
 
-  return (
-    <div className='quiz-app'>
+    return (
+  <div className='quiz-app'>
 
-      {showScore ? (
-        <div className='score-section'>
-          <h2>Your Score: {score}/{QuestionData.length}</h2>
-          <button onClick={handleRestartQuiz}>Restart</button>
-        </div>) :
-        (<div className="question-section">
-          <h2>Question {currrentQuestion + 1}</h2>
-          <p>{QuestionData[currrentQuestion].question}</p>
-          <div className="options">
-            {QuestionData[currrentQuestion].options.map((option, index) => (
-              <button key={index} onClick={() => handleAnswerClick(option)}>{option}</button>
-            ))}
-          </div>
-          <div className='timer'>Time left: {timer}s </div>
-        </div>)
-      }
-    </div>
-  )
+    {!startQuiz ? (
+      <div className='start-section'>
+        <h1>Quiz App</h1>
+        <button className='start-btn' onClick={handleStartQuiz}>
+          Start Now
+        </button>
+      </div>
+
+    ) : showScore ? (
+
+      <div className='score-section'>
+        <h2>Your Score: {score}/{QuestionData.length}</h2>
+
+        <button onClick={handleRestartQuiz}>
+          Restart
+        </button>
+      </div>
+
+    ) : (
+
+      <div className="question-section">
+        <h2>Question {currrentQuestion + 1}</h2>
+
+        <p>{QuestionData[currrentQuestion].question}</p>
+
+        <div className="options">
+          {QuestionData[currrentQuestion].options.map((option, index) => (
+            <button
+              key={index}
+              onClick={() => handleAnswerClick(option)}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+
+        <div className='timer'>
+          Time left: {timer}s
+        </div>
+      </div>
+
+    )}
+
+  </div>
+)
 }
 
 export default App
